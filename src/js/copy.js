bac04d6f0988d30f109e99237a020b140643a189
Kazuki Negoro <negokaz@gmail.com>
2018-01-15 12:59:44 +0900

    fix to fail to clip large page

--- src/js/copy.js
+++ src/js/copy.js
@@ -3,12 +3,15 @@
 	var textarea = document.createElement("textarea");
   var title = document.title;
   textarea.addEventListener("paste", (event) => {
-    event.preventDefault();
     var html = event.clipboardData.getData("text/html");
     chrome.runtime.sendMessage({ method: "copy", body: html, title: title });
+    event.preventDefault();
   });
 	document.body.appendChild(textarea);
-	textarea.select();
-	document.execCommand("paste");
-	document.body.removeChild(textarea);
+  textarea.select();
+  // workaround to prevent clipboardData.getData returns blank.
+  setTimeout(() => {
+    document.execCommand("paste");
+    document.body.removeChild(textarea);
+  }, 0);
 }
